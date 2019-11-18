20950681e54109b552f6a765ca309ccf9d8011c3
Sam Bernal <sambernaldamasco@gmail.com>
2019-11-18 15:35:42 -0500

    controllers and models for posts done

--- server.js
+++ server.js
@@ -2,11 +2,14 @@
 // DEPENDENCIES
 // ========================
 const express = require("express")
+const mongoose = require("mongoose")
+const postsController = require("./controllers/posts.js")
 
 // ========================
 // GLOBAL CONFIG
 // ========================
 const app = express()
+const db = mongoose.connection
 
 // ========================
 // MIDDLEWARE
@@ -16,18 +19,23 @@ app.use(express.json())
 app.use(express.static('public'))
 
 //==== controllers
+app.use("/posts", postsController)
 
-
-// ========================
-// TEST ROUTES
-// ========================
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+// // ========================
+// // TEST ROUTES
+// // ========================
+// app.get('/', (req, res) => {
+//   res.send('hello world')
+// })
 
 // ========================
 // CONNECTIONS
 // ========================
+mongoose.connect("mongodb://localhost:27017/pairprogramming", {useNewUrlParser: true, useUnifiedTopology: true})
+
+db.once("open", () => {
+  console.log("connected to mongoose")
+});
 app.listen(3000, () => {
   console.log("listening..")
 });
